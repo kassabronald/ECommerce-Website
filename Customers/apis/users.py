d3ee8c3d9cdf8ad8c47e6d1b44e1eeaf9db274a4
Ronald Kassab <66827298+kassabronald@users.noreply.github.com>
2023-11-24 15:25:42 +0200

    Fixed an issue related to docker

--- Customers/apis/users.py
+++ Customers/apis/users.py
@@ -53,64 +53,110 @@ def add_customer():
     db.session.add(user)
     db.session.commit()
 
-    return jsonify(user_schema.dump(user)), 201
+    return (
+        jsonify({"message": "User added successfully", "user": user_schema.dump(user)}),
+        201,
+    )
 
 
 def delete_user(username):
-    user = User.query.get(username)
+    user = User.query.filter_by(username=username).first()
     if not user:
-        abort(404)
+        return jsonify({"message": f"User '{username}' not found"}), 404
 
     db.session.delete(user)
     db.session.commit()
 
-    return jsonify(user_schema.dump(user)), 200
+    return jsonify({"message": f"User '{username}' deleted successfully"}), 200
 
 
 def get_all_users():
     users = User.query.all()
-    return jsonify(user_schema.dump(users, many=True)), 200
+    return (
+        jsonify(
+            {
+                "message": "List of all users",
+                "users": user_schema.dump(users, many=True),
+            }
+        ),
+        200,
+    )
 
 
 def get_user(username):
     user = User.query.filter_by(username=username).first()
     if not user:
-        abort(404)
+        return jsonify({"message": f"User '{username}' not found"}), 404
 
-    return jsonify(user_schema.dump(user)), 200
+    return (
+        jsonify(
+            {
+                "message": f"Details for user '{username}'",
+                "user": user_schema.dump(user),
+            }
+        ),
+        200,
+    )
 
 
 def deduce_money(username):
     user = User.query.filter_by(username=username).first()
     if not user:
-        abort(404)
+        return jsonify({"message": f"User '{username}' not found"}), 404
 
-    amount_to_charge = request.json["amount_to_charge"]
+    amount_to_charge = request.json.get("amount_to_charge")
+    if not isinstance(amount_to_charge, (int, float)) or amount_to_charge <= 0:
+        return jsonify({"message": "Invalid amount to charge"}), 400
+
     user.balance -= amount_to_charge
     db.session.commit()
 
-    return jsonify(user_schema.dump(user)), 200
+    return (
+        jsonify(
+            {
+                "message": f"Deducted {amount_to_charge} from user '{username}'",
+                "user": user_schema.dump(user),
+            }
+        ),
+        200,
+    )
 
 
 def charge_customer():
-    user_to_charge = request.json["user_to_charge"]
-    amount_to_charge = request.json["amount_to_charge"]
+    user_to_charge = request.json.get("user_to_charge")
+    amount_to_charge = request.json.get("amount_to_charge")
+
+    if not user_to_charge or not amount_to_charge:
+        return (
+            jsonify(
+                {"message": "Both user_to_charge and amount_to_charge are required"}
+            ),
+            400,
+        )
 
     user = User.query.filter_by(username=user_to_charge).first()
     if not user:
-        abort(404)
+        return jsonify({"message": f"User '{user_to_charge}' not found"}), 404
 
     user.balance += amount_to_charge
     db.session.commit()
 
-    return jsonify(user_schema.dump(user)), 200
+    return (
+        jsonify(
+            {
+                "message": f"Charged {amount_to_charge} to user '{user_to_charge}'",
+                "user": user_schema.dump(user),
+            }
+        ),
+        200,
+    )
 
 
 def update_information(username):
     user = User.query.filter_by(username=username).first()
 
     if not user:
-        return jsonify({"message": f"User with username {username} not found"}), 404
+        return jsonify({"message": f"User '{username}' not found"}), 404
 
     update_fields = request.json
 
@@ -118,11 +164,19 @@ def update_information(username):
         if hasattr(user, key) and key != "username" and key != "balance":
             setattr(user, key, value)
         else:
-            return jsonify({"message": f"Invalid field {key}"}), 400
+            return jsonify({"message": f"Invalid field '{key}'"}), 400
 
     db.session.commit()
 
-    return jsonify(user_schema.dump(user)), 200
+    return (
+        jsonify(
+            {
+                "message": f"User '{username}' information updated",
+                "user": user_schema.dump(user),
+            }
+        ),
+        200,
+    )
 
 
 @users.route("/", methods=["POST"], strict_slashes=False)
--- Customers/app.py
+++ Customers/app.py
@@ -39,4 +39,4 @@ app.register_blueprint(users)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="0.0.0.0")
